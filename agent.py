0cf2c1cbe9e36e4410331f3f317ce25552e76c5c
Philipp <pskudlik@gmail.com>
2019-07-27 15:35:37 +0200

    commenting

--- agent.py
+++ agent.py
@@ -2,29 +2,59 @@ import pygame
 import numpy as np
 import random
 import math
-
 pygame.init()
+
+COLORS = {
+    "RED":  (255, 50, 50),
+    "ORANGE": (200, 100, 50),
+    "GREEN": (0, 255, 50),
+    "CYAN": (0, 255, 255),
+    "MAGENTA": (255, 0, 255),
+    "BLUE": (50, 50, 255),
+    "GREY": (200, 200, 200),
+    "LIGHTGREY": (100, 100, 100),
+    "YELLOW": (255, 255, 0),
+    "TRANS": (1, 1, 1),
+    "WHITE": (255, 255, 255),
+    "BLACK": (0, 0, 0),
+}
 
 class Agent(object):
     def  __init__(self,position):
         self.position = np.array(position, dtype=float)
         self.origin = np.array(position)
-        self.size = 10
+        self.size = 15
 
-        self.color = (random.random() * 155, random.random() * 155, random.random() * 155)
+        self.color = (random.random() * 128, random.random() * 128, random.random() * 128)
         self.velocity = np.array((random.random()*2 -1,random.random()*2 -1))
         self.acceleration = np.zeros(2)
-
 
         self.maxforce = 1
         self.maxvelocity= 4
 
+        self.cohesion_radius = self.size *10
+        self.separation_radius = self.size *2
+        self.alignment_radius = self.size *2
+        self.clear_proximity_list()
+
+    def clear_proximity_list(self):
+        self.proximity_list = []
+
+    def proximity(self, agents):
+        '''
+        calculates the distance to the given list of other agents. If distance isbelow the maximum sensing range,
+        the other agent is added to the own proximity list
+        '''
+        for other_agent in agents:
+            distance = np.linalg.norm(other_agent.position - self.position)
+            if distance < max(self.cohesion_radius, self.alignment_radius,self.separation_radius):
+                self.proximity_list.append(other_agent)
+                other_agent.proximity_list.append(self)
 
     def seek(self,target_pos):
         steer = np.zeros(2)
         #difference to target
         desired = np.array(target_pos) - self.position
-
         #normalization
         if np.linalg.norm(desired) > 0:
             desired = desired/np.linalg.norm(desired)
@@ -75,7 +105,6 @@ class Agent(object):
             if np.linalg.norm(steer) > self.maxforce:
                 steer = self.normalizeto(steer,self.maxforce)
         return steer
-
 
     def cohesion(self, agents):
         cohesion_radius = self.size * 10
@@ -141,90 +170,119 @@ class Agent(object):
             steer = self.normalizeto(steer, self.maxforce)
         return steer
 
-
-
-    def flock(self, agents, weights):
-        '''
-        currently still bugged
-        '''
+    def clear_view(self, agents):
+        '''still in experimental mode'''
+        front_pos = self.position + (self.velocity * self.size//self.maxvelocity)
+        #self_distance = np.linalg.norm(self.position - front_pos)
         steer = np.zeros(2)
-        a_weight = weights[0]/100
-        c_weight = weights[1]/100
-        s_weight = weights[2]/100
+        desired = np.zeros(2)
+        is_in_front = 0
 
-        c_steer = np.zeros(2)
-        c_desired = np.zeros(2)
-        c_proximity = 0
-        c_pos_sum = np.zeros(2)
-        cohesion_radius = self.size * 10
-
-        s_steer = np.zeros(2)
-        s_desired = np.zeros(2)
-        s_difference = (0,0)
-        s_proximity = 0
-        separation_radius = self.size * 2
-
-        a_steer = np.zeros(2)
-        a_desired = np.zeros(2)
-        a_proximity  = 0
-        a_alignmentspeed = 0
-        alignment_radius = self.size * 2
+        right = (self.position[0] + self.velocity[1] * -1, self.position[1] + self.velocity[0] * 1)
+        left = (self.position[0] + self.velocity[1] * 1, self.position[1] + self.velocity[0] * -1)
 
         for other in agents:
-            distance = np.linalg.norm(other.position - self.position)
-            if distance < cohesion_radius:
-                c_pos_sum += other.position
-                c_proximity += 1
+            distance = np.linalg.norm(other.position - front_pos)
+            print(distance)
+            if distance < self.size*2:
+                is_in_front += 1
 
-            if distance < separation_radius and distance > 0:
-                s_difference = self.position - other.position
-                s_difference = self.normalizeto(s_difference, 1)
-                s_difference /= distance
-                s_difference *= other.size
-                s_desired += s_difference
-                s_proximity += 1
-
-            if distance < alignment_radius:
-                a_alignmentspeed += np.linalg.norm(other.velocity)
-                a_desired += other.velocity
-                a_proximity  += 1
-
-        if c_proximity > 0:
-            c_pos_sum /= c_proximity
-            c_desired =  c_pos_sum - self.position
-            c_desired = self.normalizeto(c_desired, self.maxvelocity)
-            c_steer = c_desired - self.velocity
-
-        if s_proximity > 0:
-            s_desired /= s_proximity
-            s_desired = self.normalizeto(s_desired, self.maxvelocity)
-            s_steer = s_desired - self.velocity
-
-        if a_proximity  > 0:
-            a_alignmentspeed /= a_proximity
-            a_desired /= a_proximity
-            a_desired = self.normalizeto(a_desired, self.maxvelocity)
-            # desired = self.normalizeto(desired, alignmentspeed)
-            a_steer = a_desired - self.velocity
-
-        steer = c_steer*c_weight + s_steer*s_weight + a_steer*a_weight
-
+        if is_in_front > 1:
+            print("isinfront")
+            desired = left if random.random()>0.5 else right
+            desired = self.normalizeto(desired, self.maxvelocity)
+            steer = desired - self.velocity
         if np.linalg.norm(steer) > self.maxforce:
             steer = self.normalizeto(steer, self.maxforce)
         return steer
 
+    def flock(self, agents):
+        '''
+        combines
+        * cohesionn
+        * alignment
+        * separation
+
+        goal was to have a single function instead of having to check distances between all agents three times.
+        '''
+        c_steer = np.zeros(2)
+        c_desired = np.zeros(2)
+        c_pos_sum = np.zeros(2)
+        c_proximity = 0
+        s_steer = np.zeros(2)
+        s_desired = np.zeros(2)
+        s_difference = (0,0)
+        s_proximity = 0
+        a_steer = np.zeros(2)
+        a_desired = np.zeros(2)
+        a_alignmentspeed = 0
+        a_proximity = 0
+        #check all agents in proximity list (when calculated beforehand) or simply in full agent list
+        #for other_agent in self.proximity_list:
+        for other_agent in agents:
+            distance = np.linalg.norm(other_agent.position - self.position)
+            if distance < self.cohesion_radius:
+                c_pos_sum += other_agent.position
+                c_proximity += 1
+            if distance < self.separation_radius and distance > 0:
+                s_difference = self.position - other_agent.position
+                s_difference = self.normalizeto(s_difference, 1)
+                s_difference /= distance
+                s_difference *= self.size ###!!
+                s_desired += s_difference
+                s_proximity += 1
+            if distance < self.alignment_radius:
+                a_alignmentspeed += np.linalg.norm(other_agent.velocity)
+                a_desired += other_agent.velocity
+                a_proximity += 1
+        #check of cohesion, or separation or alignment are to be applied
+        if c_proximity:
+            c_pos_sum /= c_proximity
+            c_desired =  c_pos_sum - self.position
+            c_desired = self.normalizeto(c_desired, self.maxvelocity)
+            c_steer = c_desired - self.velocity
+            if np.linalg.norm(c_steer) > self.maxforce:
+                c_steer = self.normalizeto(c_steer, self.maxforce)
+        if s_proximity:
+            s_desired /= s_proximity
+            s_desired = self.normalizeto(s_desired, self.maxvelocity)
+            s_steer = s_desired - self.velocity
+            if np.linalg.norm(s_steer) > self.maxforce:
+                s_steer = self.normalizeto(s_steer, self.maxforce)
+        if a_proximity:
+            a_alignmentspeed /= a_proximity
+            a_desired /= a_proximity
+            a_desired = self.normalizeto(a_desired, self.maxvelocity)
+            # a_desired = self.normalizeto(desired, alignmentspeed)
+            a_steer = a_desired - self.velocity
+            if np.linalg.norm(a_steer) > self.maxforce:
+                a_steer = self.normalizeto(a_steer, self.maxforce)
+        return c_steer, s_steer, a_steer
+
 
     def applyForce(self, force):
+        '''
+        function to apply a force to an agent
+        '''
+        self.forces.append(force)
         self.acceleration += force
 
 
-    def update(self, agents, weights=[100,70,40]):
+    def update(self, agents, weights=(100,70,40)):
+        '''
+        function that handles an agents behavior
+        :param agents: list
+            list of all agents
+        :param weights: list
+            list or tuple of the three weights for cohesion, separation and alignment
+        '''
         fleeforce=np.zeros(2)
         arriveforce=np.zeros(2)
         seekforce=np.zeros(2)
         alignmentforce=np.zeros(2)
         separationforce=np.zeros(2)
         cohesionforce=np.zeros(2)
+        self.forces = []
 
         alignment_weight = weights[0]/100
         separation_weight = weights[1]/100
@@ -232,15 +290,17 @@ class Agent(object):
 
         #calculate forces
         fleeforce = self.flee(pygame.mouse.get_pos())
-        alignmentforce = self.align(agents)
-        separationforce = self.separate(agents)
-        cohesionforce = self.cohesion(agents)
+        #alignmentforce = self.align(agents)
+        #separationforce = self.separate(agents)
+        #cohesionforce = self.cohesion(agents)
+        cohesionforce, separationforce, alignmentforce = self.flock(agents)
 
-        #apply forces
+        #weigh and apply forces
         self.applyForce(fleeforce)
         self.applyForce(alignmentforce*alignment_weight)
         self.applyForce(separationforce*separation_weight)
         self.applyForce(cohesionforce*cohesion_weight)
+
 
         #get new velocity
         self.velocity += self.acceleration
@@ -252,23 +312,34 @@ class Agent(object):
         self.acceleration *= 0
 
         #HANDLE THE EDGE
-        #1. overflow
+        #1. overflow (
         #self.position[0] = self.position[0]% 1200
         #self.position[1] = self.position[1] % 800
         #2. hard fence (reset position)
         #self.position[0] = max(0,min(self.position[0],1200))
         #self.position[1] = max(0,min(self.position[1],800))
-        #3. soft fence (seek center)
+        #3. soft fence (seek center when leaving bounds)
         if self.position[0] < 0 or self.position[1] < 0 or self.position[0] > 1200 or self.position[1] > 800:
-            self.applyForce((self.seek((600,400)) * (abs(self.position[0])%1200) * (abs(self.position[1])%800)) *0.5)
+            correction_force = (self.seek((600,400)) * (abs(self.position[0])%1200) * (abs(self.position[1])%800)) *0.5
+            self.applyForce(correction_force)
+
+    def draw_forces(self, window):
+        '''
+        function to plot all apllied forces vectors
+        '''
+        for force, color in zip(self.forces, list(COLORS.items())[:-len(self.forces)]):
+            force_end =  self.position+(50*force)
+            pygame.draw.line(window, color[1], self.position, force_end, 1)
+
+
+    def draw_velocity(self, window):
+        pygame.draw.line(window, self.multiply_color(self.color,0.5), self.position, self.position+(self.velocity*self.size//self.maxvelocity), 1)
 
 
     def draw(self, window):
-        # draw character blob
-
-        #pygame.draw.circle(window, self.color, (int(self.position[0]),int(self.position[1])), int(self.size),2)
-        #pygame.draw.circle(window, self.color, self.origin, int(self.size/3), 2)
-
+        '''
+        function that draws the agent
+        '''
         tip = self.normalizeto(self.velocity, self.size)
         tiplineEnd = (self.position[0] + tip[0],
                    self.position[1] + tip[1])
@@ -300,3 +371,6 @@ class Agent(object):
             return (vector/np.linalg.norm(vector)) * max
         else:
             return np.zeros(2)
+
+    def multiply_color(self, color, value):
+        return (color[0]*value, color[1]*value, color[2]*value)
--- main.py
+++ main.py
@@ -15,10 +15,8 @@ window = pygame.display.set_mode((win_width,win_height))
 pygame.display.set_caption("Steering bahavior")
 window.fill(bg_color)
 
-
 clock = pygame.time.Clock()
 run = True
-
 
 #screen elements
 align = Slider("Alignment", 70, 0, 100, 20, 20)
@@ -26,10 +24,9 @@ sepa = Slider("Separation", 100, 0, 100, 20, 65)
 cohe = Slider("Cohesion", 40, 0, 100, 20, 110)
 sliders = [align, sepa, cohe]
 
-
+#initial agents
 agents = []
-
-for i in range(1):
+for i in range(20):
     agents.append(Agent((int(random.random()*1600),int(random.random()*900))))
 
 while(run):
@@ -40,38 +37,47 @@ while(run):
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-
         elif event.type == pygame.MOUSEBUTTONDOWN:
             agents.append(Agent(mouse_pos))
-
             # slider
             for s in sliders:
                 if s.button_rect.collidepoint(mouse_pos):
                     s.hit = True
-
         elif event.type == pygame.MOUSEBUTTONUP:
             for s in sliders:
                 s.hit = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_r:
+                agents = agents[:-1]
 
-    weigths = [s.val for s in sliders]
-
-
-    for agent in agents:
-        agent.update(agents, weigths)
-        agent.draw(window)
-
-
-
+    #weights and slider settings
+    weights = [s.val for s in sliders]
     # Move sliders
     for s in sliders:
         if s.hit:
             s.move()
         s.draw(window)
 
+    #for i, agent in enumerate(agents):
+    #   #agent.proximity(agents[i:])
+    #   agent.proximity(agents[i+1:])
+
+    # iterate over all agents to calculate the behavior
+    for agent in agents:
+        agent.update(agents, weights)
+        agent.draw(window)
+        # agent.clear_proximity_list()
+
+    # draw instruction
+    #instr1 = sys_font.render("Instructions:" , 0, (0, 0, 0))
+    instr2 = sys_font.render("click to spawn new agent" , 0, (0, 0, 0))
+    instr3 = sys_font.render("press R to remove agent" , 0, (0, 0, 0))
+    #window.blit(instr1, (1000, 30))
+    window.blit(instr2, (1000, 30))
+    window.blit(instr3, (1000, 50))
 
     pygame.display.update()
     clock.tick(90)
-
 
 pygame.quit()
 quit()
